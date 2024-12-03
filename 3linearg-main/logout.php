25c10af624197cc9ff63288fce20acb739591e7f
fallavaquerogonzales-_- <163131184+matifaya@users.noreply.github.com>
2024-12-03 15:20:31 -0300

    Delete 3linearg-main directory

--- 3linearg-main/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("conexion.php");
-
-$stmt = $mysqli->prepare("UPDATE usuario SET sesion='desconectado' WHERE id=$id_usr_conectado");
-$stmt->execute();
-
-header("Location: pagina.php");
-
-?>
